dba2c50da76a1209cf6047f5ec13619448943834
Sviatoslav <lalkabagi@gmail.com>
2022-07-30 15:06:05 +0300

    v1

---
+++ main.py
@@ -0,0 +1,9 @@
+n = int(input("Скільки гривень хочете вкласти:"))
+years = int(input("На скільки років хочете вкласти:"))
+
+for i in range(years):
+    n += n*0.10
+
+def bank(n,years):
+    print(f"{n}")
+print(bank(n,years))
